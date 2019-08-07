fc12b0410244fb49f4be03dcc70be2ae43884742
ChiLam <raymond.nguyen9x@gmail.com>
2019-08-07 13:46:12 +0700

    fix uncache

--- resources/views/admin/footer.blade.php
+++
@@ -1,8 +0,0 @@
-<audio id="notiAudio">
-    <source src="{{ asset('sounds/chat.mp3') }}">
-</audio>
-<div class="pull-right hidden-xs">
-    <b>Version</b> 0.0.1
-</div>
-<strong>Copyright &copy; 2018 <a href="{{ url('') }}">IHT GO</a>.</strong> All rights
-reserved.
