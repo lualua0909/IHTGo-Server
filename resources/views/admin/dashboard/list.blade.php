6de1ef6648ac9fa02fdeacd1543502b267f30d33
Chi Lam <raymond.nguyen9x@gmail.com>
2019-08-26 14:15:01 +0700

    fix dashboard

--- resources/views/admin/dashboard/list.blade.php
+++ resources/views/admin/dashboard/list.blade.php
@@ -17,7 +17,7 @@
                             <div class="icon">
                                 <i class="ion ion-bag"></i>
                             </div>
-                            <a href="{{route('order.list')}}" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                            <a href="{{route('order.list.new')}}" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
                         </div>
                     </div>
                     <!-- ./col -->
