3b43bb594f92e2bfad67241396f0d946fded6e21
Chi Lam <raymond.nguyen9x@gmail.com>
2019-08-30 14:44:44 +0700

    fix error delete order

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -520,8 +520,8 @@ class Order extends BaseModel
         $user_id = Auth::user()->id;
         if ($user_id == 1) {
             DB::table('orders')->where('id', $id)->where('updated_at', null)->delete();
-            DB::table('order_details')->where('updated_at', null)->where('order_id', $id)->delete();
-            DB::table('order_detail_ext')->where('updated_at', null)->where('order_id', $id)->delete();
+            DB::table('order_details')->where('order_id', $id)->delete();
+            DB::table('order_detail_ext')->where('order_id', $id)->delete();
             return 200;
         } else {
             return 201;
